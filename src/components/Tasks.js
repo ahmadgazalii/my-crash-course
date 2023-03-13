980afa56f0b8a27a10061381766c0267e3615512
ahmadgazalii <aghazali489@gmail.com>
2023-03-13 20:31:42 +0500

    react crash course

---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import Task from './Tasks'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  )
+}
+
+export default Tasks
